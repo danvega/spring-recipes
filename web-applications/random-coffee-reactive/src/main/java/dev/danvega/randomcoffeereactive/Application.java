266a1adc6260e49977cf678b68d19ce654a4bec1
Dan Vega <danvega@gmail.com>
2022-08-03 13:58:49 -0400

    Spring Reactive Web Client demo

--- web-applications/random-coffee-reactive/src/main/java/dev/danvega/randomcoffeereactive/Application.java
+++ web-applications/random-coffee-reactive/src/main/java/dev/danvega/randomcoffeereactive/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RandomCoffeeReactiveApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RandomCoffeeReactiveApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ web-applications/random-coffee-reactive/src/main/java/dev/danvega/randomcoffeereactive/model/Coffee.java
@@ -0,0 +1,5 @@
+package dev.danvega.randomcoffeereactive.model;
+
+public record Coffee(Integer id, String file) {
+
+}
