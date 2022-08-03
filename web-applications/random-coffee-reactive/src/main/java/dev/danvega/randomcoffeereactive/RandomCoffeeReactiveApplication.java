829e9275291c8f001b32234d41aa163764138410
Dan Vega <danvega@gmail.com>
2022-08-03 12:03:02 -0400

    Spring Webflux Web Client demo

---
+++ web-applications/random-coffee-reactive/src/main/java/dev/danvega/randomcoffeereactive/RandomCoffeeReactiveApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega.randomcoffeereactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomCoffeeReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomCoffeeReactiveApplication.class, args);
+	}
+
+}
