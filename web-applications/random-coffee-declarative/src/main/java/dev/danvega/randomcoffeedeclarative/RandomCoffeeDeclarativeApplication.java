7a702d245cb2a272c1bd1ec912068d90c9f35f81
Dan Vega <danvega@gmail.com>
2022-08-03 14:00:07 -0400

    Declarative http clients demo

---
+++ web-applications/random-coffee-declarative/src/main/java/dev/danvega/randomcoffeedeclarative/RandomCoffeeDeclarativeApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega.randomcoffeedeclarative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomCoffeeDeclarativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomCoffeeDeclarativeApplication.class, args);
+	}
+
+}
---
+++ web-applications/random-coffee-declarative/src/test/java/dev/danvega/randomcoffeedeclarative/RandomCoffeeDeclarativeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.randomcoffeedeclarative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCoffeeDeclarativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
