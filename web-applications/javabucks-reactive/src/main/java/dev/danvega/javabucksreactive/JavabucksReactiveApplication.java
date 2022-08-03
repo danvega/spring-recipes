c872f9c4464e32eae1245ca66aac2c36f2966a88
Dan Vega <danvega@gmail.com>
2022-08-03 10:30:02 -0400

    starter project for reactive web demo

---
+++ web-applications/javabucks-reactive/src/main/java/dev/danvega/javabucksreactive/JavabucksReactiveApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega.javabucksreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabucksReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabucksReactiveApplication.class, args);
+	}
+
+}
---
+++ web-applications/javabucks-reactive/src/test/java/dev/danvega/javabucksreactive/JavabucksReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.javabucksreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabucksReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
