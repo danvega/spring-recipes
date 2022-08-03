931c0fdef31d6ae1d1bf13d04b75b5da164138d1
Dan Vega <danvega@gmail.com>
2022-08-03 16:12:13 -0400

    Adding basic database connection with h2

---
+++ databases/h2-demo/src/main/java/dev/danvega/h2demo/H2DemoApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega.h2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DemoApplication.class, args);
+	}
+
+}
