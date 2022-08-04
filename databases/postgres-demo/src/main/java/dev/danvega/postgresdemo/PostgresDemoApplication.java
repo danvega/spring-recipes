39bc0ff8571eb208cc030fa0bbeef2491bd27a5a
Dan Vega <danvega@gmail.com>
2022-08-04 10:16:49 -0400

    postgres demo initial commit

---
+++ databases/postgres-demo/src/main/java/dev/danvega/postgresdemo/PostgresDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega.postgresdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresDemoApplication.class, args);
+	}
+
+}
