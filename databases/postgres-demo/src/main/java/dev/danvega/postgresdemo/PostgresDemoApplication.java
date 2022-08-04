8b93ba7994a519612e242efe2bd605a888eaa024
Dan Vega <danvega@gmail.com>
2022-08-04 10:58:50 -0400

    connecting to and schema generation

--- databases/postgres-demo/src/main/java/dev/danvega/postgresdemo/PostgresDemoApplication.java
+++ databases/postgres-demo/src/main/java/dev/danvega/postgresdemo/PostgresDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PostgresDemoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PostgresDemoApplication.class, args);
 	}
